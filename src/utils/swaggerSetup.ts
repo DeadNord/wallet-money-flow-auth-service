401b8dc01fd52b6667f6085f46fa98b8a701a339
Yegor Osipov <83457266+DeadNord@users.noreply.github.com>
2024-03-27 21:03:47 +0100

    improve for prod

--- src/utils/swaggerSetup.ts
+++ src/utils/swaggerSetup.ts
@@ -13,22 +13,22 @@ const NODE_ENV = process.env.NODE_ENV;
  */
 export function setupSwagger(app: INestApplication): void {
   // Enable Swagger only in development mode for security reasons
-  if (NODE_ENV === 'development') {
-    // Configure the Swagger documentation builder with information about your API
-    const options = new DocumentBuilder()
-      .setTitle('Auth-Service') // Title of the API
-      .setDescription('The Auth-Service API description') // Short description or summary of the API
-      .setVersion('1.0') // Version of the API
-      .addBearerAuth(
-        { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' }, // Authentication configuration
-        'access-token', // Identifier for the security scheme in documentation
-      )
-      .build(); // Build the Swagger options
+  // if (NODE_ENV === 'development') {
+  // Configure the Swagger documentation builder with information about your API
+  const options = new DocumentBuilder()
+    .setTitle('Auth-Service') // Title of the API
+    .setDescription('The Auth-Service API description') // Short description or summary of the API
+    .setVersion('1.0') // Version of the API
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' }, // Authentication configuration
+      'access-token', // Identifier for the security scheme in documentation
+    )
+    .build(); // Build the Swagger options
 
-    // Create Swagger documentation based on the setup options
-    const document = SwaggerModule.createDocument(app, options);
+  // Create Swagger documentation based on the setup options
+  const document = SwaggerModule.createDocument(app, options);
 
-    // Serve the Swagger UI on the '/docs/swagger' path
-    SwaggerModule.setup('docs/swagger', app, document);
-  }
+  // Serve the Swagger UI on the '/docs/swagger' path
+  SwaggerModule.setup('docs/swagger', app, document);
+  // }
 }
