2e47d25d6293d07cd6d7b33a38944e13044dba5b
Yegor Osipov <83457266+DeadNord@users.noreply.github.com>
2024-03-30 13:29:53 +0100

    fix toml & clean swagger

--- src/utils/swaggerSetup.ts
+++ src/utils/swaggerSetup.ts
@@ -2,9 +2,6 @@
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-// Directly use the environment variable within the file
-const NODE_ENV = process.env.NODE_ENV;
-
 /**
  * Sets up Swagger documentation for the application.
  * This is typically used in development environments to provide an interactive API documentation.
@@ -12,8 +9,6 @@ const NODE_ENV = process.env.NODE_ENV;
  * @param {INestApplication} app - The instance of the NestJS application.
  */
 export function setupSwagger(app: INestApplication): void {
-  // Enable Swagger only in development mode for security reasons
-  // if (NODE_ENV === 'development') {
   // Configure the Swagger documentation builder with information about your API
   const options = new DocumentBuilder()
     .setTitle('Auth-Service') // Title of the API
@@ -30,5 +25,4 @@ export function setupSwagger(app: INestApplication): void {
 
   // Serve the Swagger UI on the '/docs/swagger' path
   SwaggerModule.setup('api/auth/swagger', app, document);
-  // }
 }
